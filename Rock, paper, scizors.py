0b34d1c8f42551d108edaa137bab22d742004f76
Dominika Jastrzebska <d.jastrzebska@sysbreakers.com>
2017-04-09 22:28:32 +0200

    typo mod.

--- Rock, paper, scizors.py
+++ Rock, paper, scizors.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import random
 
 
@@ -34,54 +32,3 @@ try:
     rand_choice.compare(dict_element)
 except KeyError:
     print('Choose element from list: )')
-
-
-class Score:
-    '''Write score to file with date and hour of play'''
-    
-        
-    
-=======
-
-import random
-
-
-class Element:
-    
-    def __init__(self, name, win, lose):
-        self.name = name
-        self.win = win
-        self.lose = lose
-
-    def compare(self, other):
-        if isinstance(other, type(self)):
-            pass
-        if self.name == other.name:
-            print('dead-head')
-        elif self.name in other.win:
-            print('You won')
-        else:
-            print('You lost')
-
-rock = Element('rock', ['scizors'], ['paper'])
-paper = Element('paper', ['rock'], ['scizors'])
-scizors = Element('scizors', ['paper'], ['rock'])
-
-element_list = [paper, rock, scizors]
-rand_choice = random.choice(element_list)
-print(rand_choice.name)
-user = input('Choose an element (rock, paper, scizors): ')
-
-try:
-    dict_element = {'paper': paper, 'rock':rock, 'scizors': scizors}[user]
-    rand_choice.compare(dict_element)
-except KeyError:
-    print('Choose element from list: )')
-
-
-class Score:
-    '''Write score to the file with date and hour of play'''
-    
-        
-    
->>>>>>> 93a44450da187e696fbe125b66869c4554ce8e97
